b09b7016827f77e7746d792493c8cff21fd6e6f0
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-03 13:37:33 +0100

    Update objects.js

--- objects.js
+++ objects.js
@@ -23,3 +23,29 @@ console.log(robot.recharge())
 
 // This will change the robot energy level by reassigning it in the object. This will print Recharged! Energy is currently at 60%.
 
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if (typeof num === 'number' && num >= 0)
+    {
+      this._numOfSensors = num
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0')
+    }
+  }
+  
+};
+robot.numOfSensors = 100;
+
+console.log(robot.numOfSensors)
+
+// With get and set methods (one returns, one changes)
