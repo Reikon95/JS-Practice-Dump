69fe4a7ddba30181b3f78aefdcc362c648bd9ea8
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-04 09:48:50 +0100

    Update array-methods.js

--- array-methods.js
+++ array-methods.js
@@ -11,3 +11,13 @@ function squareNums(arr) {
 console.log(squareNums(numbers))
 
 //Output is [ 4, 49, 81, 29241, 2704, 1089, 196 ]
+//Another example w/strings
+function shoutGreetings(arr){
+  const result = arr.map(x => x.toUpperCase() + '!')
+  return result
+}
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
+//output is [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
