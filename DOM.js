0c7fbf8c00749243037b8acc9b2e23af8732ee49
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-11 11:00:20 +0100

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -4,3 +4,7 @@ document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016'
 document.getElementById('fourth').innerHTML = 'Fourth element';
 
 document.querySelector('body').style.backgroundColor = '#201F2E';
+
+let newDestination = document.createElement("li");
+newDestination.innerHTML = "Oaxaca, Mexico";
+document.getElementById("more-destinations").appendChild(newDestination);
