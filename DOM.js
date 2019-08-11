04bf3b1e9aae9b9ebb23d10d71cc1545c2b1ac0f
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-11 10:55:02 +0100

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -2,3 +2,5 @@ document.body.innerHTML = '<body>This is the text of the body element</body>';
 document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016';
 
 document.getElementById('fourth').innerHTML = 'Fourth element';
+
+document.querySelector('body').style.backgroundColor = '#201F2E';
