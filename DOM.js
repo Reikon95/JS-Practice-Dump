fef7a59acc1b9f797b20a3272b8d87f66eee0488
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-11 10:52:05 +0100

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -1,1 +1,4 @@
 document.body.innerHTML = '<body>This is the text of the body element</body>';
+document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016';
+
+document.getElementById('fourth').innerHTML = 'Fourth element';
