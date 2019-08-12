798b49a370379bab9a3f4084555db5e354a864ef
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-12 12:57:54 +0100

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -26,3 +26,10 @@ element.onclick = turnButtonRed;
 let first = document.body.firstChild;
 first.innerHTML = "First child";
 first.parentNode.innerHTML = "I am the parent and my inner HTML has been replaced!";
+
+// The document keyword grants access to the root of the DOM in JavaScript
+// The DOM Interface allows you to select a specific element with CSS selectors by using the .querySelector() method
+// You can also access an element directly by its ID with .getElementById()
+// The .innerHTML and .style properties allow you to modify an element by changing its contents or style respectively
+// You can create, append, and remove elements by using the .createElement(),.appendChild() and .removeChild() methods respectively
+// The .onclick property can add interactivity to a DOM element based on a click event
