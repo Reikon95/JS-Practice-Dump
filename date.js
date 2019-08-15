a7626b7ffa9d23ee750afb4bb8b1ea345b3ea8e2
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-15 09:06:13 +0100

    Update date.js

--- date.js
+++ date.js
@@ -5,3 +5,10 @@ function isValidDate(val) {
 		return false;
 	}
 }
+
+function getDayOfTheMonth(val) {
+	let date = new Date(val);
+	return date.getDate();
+}
+
+//Above returns the date eg. the 14th
