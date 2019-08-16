365f959aaf26d628262a2d0b6f9b8977c39ad623
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-16 12:46:41 +0100

    Update date.js

--- date.js
+++ date.js
@@ -12,3 +12,13 @@ function getDayOfTheMonth(val) {
 }
 
 //Above returns the date eg. the 14th
+function getFormattedTime(date) {
+	let hours = normaliseDateToken(date.getHours());
+	let minutes = normaliseDateToken(date.getMinutes());
+	let seconds = normaliseDateToken(date.getSeconds());
+	return `${hours}:${minutes}:${seconds}`;
+}
+
+function normaliseDateToken(dateToken) {
+	return dateToken < 10 ? `0${dateToken}` : `${dateToken}`;
+}
