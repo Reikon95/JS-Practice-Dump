fbf1999dd0f87b2535859b9a15162a3d03b15299
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-20 13:15:02 +0100

    Update regex.js

--- regex.js
+++ regex.js
@@ -2,10 +2,28 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
-
 //Returns true because the word 'Hello' is in myString.
+
 //Multiples
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
 //As usual | means or - you can use it to scan for multiple, this would match for any of the animals listed
+
+//Ignore case
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i;
+let result = fccRegex.test(myString);
+//This returns true regardless of the case (the i is what is known as a flag, and is code for case insensitive)
+
+//Extracting Matches
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; 
+let result = extractStr.match(codingRegex); 
+//This returns an object which is the match (in this case, coding)
+
+//Multiple matches
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; 
+let result = twinkleStar.match(starRegex); 
+//This returns multiple matches. Note that you must use the 'i' flag too for insenstivity. The g flag allows for mmultiple.
