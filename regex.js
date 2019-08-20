11e3c98f5e5dbd6fc6a9d96b4dee50d003c5a147
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-20 13:09:23 +0100

    Update regex.js

--- regex.js
+++ regex.js
@@ -1,5 +1,11 @@
+//Intro
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
 
 //Returns true because the word 'Hello' is in myString.
+//Multiples
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; 
+let result = petRegex.test(petString);
+//As usual | means or - you can use it to scan for multiple, this would match for any of the animals listed
