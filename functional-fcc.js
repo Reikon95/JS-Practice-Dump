9a4e641d1ac0e5a2fde30d8ac5347bbefe25cbb2
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-23 15:58:10 +0100

    Update functional-fcc.js

--- functional-fcc.js
+++ functional-fcc.js
@@ -1,1 +1,37 @@
+// Functional Programming: Learn About Functional Programming
+// Functional programming is a style of programming where solutions are simple, isolated functions, 
+// without any side effects outside of the function scope.
+
+/**
+ * A long process to prepare green tea.
+ * @return {string} A cup of green tea.
+ **/
+const prepareGreenTea = () => 'greenTea';
+
+/**
+ * A long process to prepare black tea.
+ * @return {string} A cup of black tea.
+ **/
+const prepareBlackTea = () => 'blackTea';
+
+/**
+ * Get given number of cups of tea.
+ * @param {function():string} prepareTea The type of tea preparing function.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+
+  return teaCups;
+};
+
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27); 
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13); 
 
