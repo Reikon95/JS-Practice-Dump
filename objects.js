d725414dfc87dbb6726a55ec6c4dfcd910bad2cf
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-09-26 12:36:18 +0100

    Update objects.js

--- objects.js
+++ objects.js
@@ -181,3 +181,34 @@ glideMixin(boat);
 
 
 //Adds the function to these objects
+
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+	  super(name);
+    this._name = name;
+    this._certifcations = certifications;
+  }
+  
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
